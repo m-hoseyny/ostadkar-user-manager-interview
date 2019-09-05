9e0ccfb1d2cc97e9e951f5fc733edfa8609650c9
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2019-09-05 15:45:55 +0430

    Add db and bcrypt

--- src/app.py
+++ src/app.py
@@ -1,22 +1,28 @@
 from flask import Flask
 
 from .config import app_config
+from .models import db, bcrypt
+
 
 def create_app(env_name):
-  """
-  Create app
-  """
-  
-  # app initiliazation
-  app = Flask(__name__)
-
-  app.config.from_object(app_config[env_name])
-
-  @app.route('/', methods=['GET'])
-  def index():
     """
-    example endpoint
+    Create app
     """
-    return 'Congratulations! Your first endpoint is workin'
+    
+    # app initiliazation
+    app = Flask(__name__)
 
-  return app
+    app.config.from_object(app_config[env_name])
+
+    bcrypt.init_app(app)
+
+    db.init_app(app)        
+
+    @app.route('/', methods=['GET'])
+    def index():
+        """
+        example endpoint
+        """
+        return 'Congratulations! Your first endpoint is workin'
+
+    return app
