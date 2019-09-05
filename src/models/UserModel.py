d83ae33d89ca3cb657af279c9343dc10f0fb89f1
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2019-09-05 15:44:39 +0430

    Modify FirstName and LastName

--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -8,7 +8,8 @@ class UserModel(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(128), nullable=False)
+    last_name = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=True)
     created_at = db.Column(db.DateTime)
