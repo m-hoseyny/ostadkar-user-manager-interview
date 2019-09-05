d7e8c075e91a6ee850e3044fa92f64d017e0e9f7
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2019-09-05 17:56:07 +0430

    Add user endpoint

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 from .config import app_config
 from .models import db, bcrypt
-
+from .views.UserView import user_api as user_blueprint
 
 def create_app(env_name):
     """
@@ -17,6 +17,7 @@ def create_app(env_name):
     bcrypt.init_app(app)
 
     db.init_app(app)        
+    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users') # add this line
 
     @app.route('/', methods=['GET'])
     def index():
