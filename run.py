ea78fe7881a4d30a6e0ff93e08e4488ef1c8f2be
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2019-09-05 15:43:40 +0430

    Add run file

---
+++ run.py
@@ -0,0 +1,16 @@
+
+import os
+
+from src.app import create_app
+import environ
+
+env = environ.Env()
+environ.Env.read_env('.env')
+
+
+
+if __name__ == '__main__':
+  env_name = os.getenv('FLASK_ENV')
+  app = create_app(env_name)
+  # run app
+  app.run()
